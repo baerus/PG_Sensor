fe1d072b0ee58ee6c962e82ea2831af94fe25d35
Marcus Fischer <xmarcus89@yahoo.de>
2013-12-16 15:57:46 +0100

    4 Tabs ..

--- www/js/general.js
+++ www/js/general.js
@@ -1,10 +1,29 @@
 function onShake() {
-    alert('Shaked!');
+    alert('Das Gerät wurde geschüttelt');
     var tab1 = document.getElementById('t1');
     var tab2 = document.getElementById('t2');
+    var tab3 = document.getElementById('t3');
+    var tab4 = document.getElementById('t4');
     // keinen mehr selektieren
     tab1.className = "";
     tab2.className = "";
-    // Tab 2 selektieren
-    tab2.className = "selected"
+    tab3.className = "";
+    tab4.className = "";
+    // Tab 3 selektieren
+    tab3.className = "selected"
+}
+
+function setTabSelected(tabId) {
+    var clickedTab = document.getElementById(tabId);
+    // keinen mehr selektieren
+    var tab1 = document.getElementById('t1');
+    var tab2 = document.getElementById('t2');
+    var tab3 = document.getElementById('t3');
+    var tab4 = document.getElementById('t4');
+    tab1.className = "";
+    tab2.className = "";
+    tab3.className = "";
+    tab4.className = "";
+    // Geklickten Tab selektieren
+    clickedTab.className = "selected";
 }
